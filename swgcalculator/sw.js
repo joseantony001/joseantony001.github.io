a082e41b5fa0a6a6c8f9a5b5c699a3f98dfa8496
joseantony001 <joseantony001@gmail.com>
2019-11-23 22:51:59 -0600

    Update sw.js

--- swgcalculator/sw.js
+++ swgcalculator/sw.js
@@ -1,1 +1,32 @@
-console.log('I am a Service Worker!');
+var CACHE_NAME = 'swg-calculator';
+var urlsToCache = [
+  '/swgcalculator/',
+  '/swgcalculator/bootstrap.min.css',
+  '/swgcalculator/bootstrap.min.js',
+  '/swgcalculator/jquery.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
